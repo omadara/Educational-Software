e4f29c71721f5e9b34877b2b5b5da18da9f46c3d
fanis1996 <fanis1996GR20@hotmail.com>
2019-05-01 06:13:50 +0300

    added the database

---
+++ Educational Software/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Educational_Software
+{
+    class Database
+    {
+        private SQLiteConnection con;
+        private string filename = "db.sqlite";
+        public Database()
+        {
+            if (!System.IO.File.Exists(filename))
+            {
+                SQLiteConnection.CreateFile(filename);
+            }
+            con = new SQLiteConnection("Data Source=" + filename + ";Version=3;");
+            con.Open();
+            string sql1 = "CREATE TABLE IF NOT EXISTS  lessons(title TEXT,times_visited INT);";
+            string sql2 = "CREATE TABLE IF NOT EXISTS  student_results(question_title TEXT,answered_correctly BOOLEAN,timestamp DATETIME);";
+            SQLiteCommand command = new SQLiteCommand(sql1+sql2, con);
+            command.ExecuteNonQuery();
+        }
+
+        //TODO Add_lesson,Update_lesson,Add_student_result
+    }
+}
--- Educational Software/Program.cs
+++ Educational Software/Program.cs
@@ -8,15 +8,18 @@ namespace Educational_Software
 {
     static class Program
     {
+        static private Database db;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            db = new Database();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
